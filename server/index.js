8f4f2a1abab46abf7ed346a9c1f01534fd0e9fa3
AArnedo <arnedoagustin@gmail.com>
2026-09-24 19:39:25 +0200

    fix: corregimos origin del cors en el servidor

--- server/index.js
+++ server/index.js
@@ -8,7 +8,13 @@ import authRouter from './routes/auth.routes.js'
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors())
+app.use(cors({
+  origin: [
+    'https://dashboard-tracking-book.vercel.app/',
+    'http://localhost:5173'
+  ],
+  credentials: true
+}));
 app.use(express.json())
 app.use('/api/auth', authRouter)
 app.use('/api/books', booksRouter)
